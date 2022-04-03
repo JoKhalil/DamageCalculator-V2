8b3a707f0095598f946c9b013a6960900f37fe11
zouz82 <jagal_zouzou@hotmail.com>
2022-04-03 18:38:29 -0400

    Move class done

--- PokemonCalculator/Generation.cs
+++ PokemonCalculator/Generation.cs
@@ -6,5 +6,16 @@ namespace PokemonCalculator
 {
     class Generation
     {
+        private int gen;
+        private double crit;
+
+        public int Gen { get => gen; set => gen = value; }
+        public double Crit { get => crit; set => crit = value; }
+
+        public Generation(int gen, double crit)
+        {
+            this.gen = gen;
+            this.crit = crit;
+        }
     }
 }
--- PokemonCalculator/Move.cs
+++ PokemonCalculator/Move.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 
 namespace PokemonCalculator
 {
+    [Serializable]
     class Move
     {
+        
+
+        private String name;
+        private String type;
+        private int powerPoint;
+        private ArrayList category;
+
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public int PowerPoint { get => powerPoint; set => powerPoint = value; }
+        public ArrayList Category { get => category; set => category = value; }
+
+        public Move(String name, String type, int powerPoint, ArrayList category)
+        {
+            this.name = name;
+            this.type = type;
+            this.powerPoint = powerPoint;
+            this.category = category;
+        }
+
+        public ArrayList jsonToMoves()
+        {
+            
+        }
     }
 }
