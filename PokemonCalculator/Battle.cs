59d69ef0092ab82f8d450b0d9220203a51b41683
zouz82 <jagal_zouzou@hotmail.com>
2022-04-03 18:41:50 -0400

    Battle class done

--- PokemonCalculator/Battle.cs
+++ PokemonCalculator/Battle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,16 @@ namespace PokemonCalculator
 {
     class Battle
     {
+        private int targetNumber;
+        private ArrayList weather;
+
+        public int TargetNumber { get => targetNumber; set => targetNumber = value; }
+        public ArrayList Weather { get => weather; set => weather = value; }
+
+        public Battle (int targetNumber, ArrayList weather)
+        {
+            this.targetNumber = targetNumber;
+            this.weather = weather;
+        }
     }
 }
